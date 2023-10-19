a91c260f915d0397f0fcdd45a8ec987a5d900872
Orshluck <126862101+Orshluck@users.noreply.github.com>
2023-10-19 11:37:33 +0200

    corrected files

--- Barwy i histogram/test.js
+++
@@ -1,8 +0,0 @@
-function preload() {
-    img = loadImage("https://raw.githubusercontent.com/scikit-image/scikit-image/master/skimage/data/astronaut.png");
-}
-
-function setup() {
-    createCanvas(512,512);
-    image(img, 0, 0);
-}
